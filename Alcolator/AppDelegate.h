bd8aaa7f6af16f45fb252fb19ff483ab219c157a
Sameer Totey <sameertotey@hotmail.com>
2014-10-07 22:15:33 -0600

    Initial Commit

---
+++ Alcolator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Alcolator
+//
+//  Created by Sameer Totey on 10/7/14.
+//  Copyright (c) 2014 Sameer Totey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Alcolator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Alcolator
+//
+//  Created by Sameer Totey on 10/7/14.
+//  Copyright (c) 2014 Sameer Totey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
